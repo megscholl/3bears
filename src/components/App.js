bf82cb5724b7f22220e552b861ea62f047b17344
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-09 22:59:34 -0500

    centered inline 3bears logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div>
           <Nav />
+          <Main />
+          <Footer />
       </div>
     );
   }
