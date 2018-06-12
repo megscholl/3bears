4a0664ea2b7db4718483d356d233e291cba80b39
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-11 23:03:39 -0500

    added info to json file with survey results, still have half to do. styled main and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Inline_logo from './images/inline_png.png';
 class Footer extends Component {
     render() {
         return(
-            <div className="center-footer">
+            <div className="footer center-footer">
                 <img src={Inline_logo} alt="3Bears inline logo" className="inline-logo"/>
                 <h5>design & development by meg scholl</h5>
                 <h6>images from unsplash</h6>
