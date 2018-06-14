706e709fcba62137457337d393f7a37483af7029
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-14 13:58:24 -0500

    add about content, and styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
     render() {
         return(
             <div>
-                <h1 className="research-h1">Research Conducted on 27 Nashville Companies</h1>
+                <h1 className="research-h1">Contact 3Bears</h1>
             </div>
         )
     }
