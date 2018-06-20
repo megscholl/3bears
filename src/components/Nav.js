2969b229dc336f5afc41b0198ca87a1c0458e375
Meg Scholl <meghan.e.scholl@gmail.com>
2018-06-19 20:40:46 -0500

    nav is responsive...would like it to collapse at some point

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import './nav.css';
-import { NavLink } from 'react-router-dom';
-import Block_logo from './images/block_png.png';
+import React, { Component } from 'react'
+import './nav.css'
+import { NavLink } from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+import Block_logo from './images/block_png.png'
 
 class Nav extends Component {
     render() {
         return(
-            <div>
+            <div className="top-padding">
                 <img src={Block_logo} alt="3Bears Block logo" className="block-logo"/>
-                <nav>
-                    <ul className="nav-bar">
-                        <li><NavLink to="/home" className="nav-link">home</NavLink></li>
-                        <li><NavLink to="/companies" className="nav-link">companies</NavLink></li>
-                        <li><NavLink to="/discover" className="nav-link">cultures</NavLink></li>
-                        <li><NavLink to="/research" className="nav-link">research</NavLink></li>
-                        <li><NavLink to="/reviews" className="nav-link">write a review</NavLink></li>
-                        <li><NavLink to="/about" className="nav-link">about</NavLink></li>
-                        <li><NavLink to="/contact" className="nav-link">contact</NavLink></li>
-                    </ul>
-                </nav>
+                <Menu className="nav-bar">
+                        <Menu.Item className="item"><NavLink to="/home" className="nav-link">home</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/companies" className="nav-link">companies</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/discover" className="nav-link">cultures</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/research" className="nav-link">research</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/reviews" className="nav-link">write a review</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/about" className="nav-link">about</NavLink></Menu.Item>
+                        <Menu.Item className="item"><NavLink to="/contact" className="nav-link">contact</NavLink></Menu.Item>
+                </Menu>
             </div>
         )
     }
